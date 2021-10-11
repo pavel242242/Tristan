72c5813383a819289be7eb1ca261b4040a324764
pavel242242 <pavel@chocholous.net>
2021-10-11 19:23:59 +0200

    x

---
+++ main/transformation/keboola.snowflake-transformation/5539862-audit-papertrail/blocks/001-block-1/001-audit-log/code.sql
@@ -0,0 +1,11 @@
+create table "AUDITLOG" as --test
+select current_timestamp() as TIMESTAMP,
+       count(*) as num_rows,
+       'data_science_sandbox' as "TABLE"
+from "ds_log";
+
+insert into "AUDITLOG"
+select current_timestamp() as TIMESTAMP,
+       count(*) as num_rows,
+       'telemetry' as "TABLE"
+from "telemetry";
